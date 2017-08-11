0364ef4ed44db64a20492e448919d1c19353c5e9
Cindy Ochoa <ochoacind@gmail.com>
2017-08-11 22:25:16 +0000

    Added before filters to chekc user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
     # GET to /users/:id
     def show
         @user = User.find( params[:id] )
    end
+
 end
    
